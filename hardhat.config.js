db0ac8e9e858a67a76b333fa46d3021b7e177ff2
Prastav <pokharelprastav@gmail.com>
2024-01-13 00:56:42 +0545

    Deployment scripts for naruto nft completed

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,50 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+require("solidity-coverage");
+require("hardhat-deploy");
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+
 module.exports = {
-  solidity: "0.8.19",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+      blockConfirmations: 6,
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+      },
+    ],
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      1: 0,
+    },
+  },
+  // mocha: {
+  //   timeout: 200000, // 200 seconds max for running tests
+  // },
 };
---
+++ helper-hardhat-config.js
@@ -0,0 +1,25 @@
+const networkConfig = {
+  31337: {
+    name: "localhost",
+    gasLane:
+      "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+    mintFee: "10000000000000000",
+    callbackGasLimit: "500000",
+  },
+  11155111: {
+    name: "sepolia",
+    vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
+    gasLane:
+      "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+    callbackGasLimit: "500000",
+    mintFee: "10000000000000000",
+    subscriptionId: "5449",
+  },
+};
+
+const developmentChains = ["hardhat", "localhost"];
+
+module.exports = {
+  networkConfig,
+  developmentChains,
+};
