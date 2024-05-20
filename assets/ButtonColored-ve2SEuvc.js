b483edce3c7497b916dbbd34747b4980a9815336
Prastav <pokharelprastav@gmail.com>
2024-05-20 14:31:53 +0545

    Updates

---
+++ assets/ButtonColored-ve2SEuvc.js
@@ -0,0 +1,89 @@
+import{x as r,y as l,z as o,B as a,D as n,E as s}from"./index-ClxLoTQ8.js";const t=r`
+    :after {
+        background-color: ${l("light",90)};
+    }
+
+    :hover {
+        :after {
+            background-color: ${l("light",70)};
+        }
+    }
+
+    :active {
+        :after {
+            background-color: ${l("light",50)};
+        }
+    }
+`,d=r`
+    background-color: ${o.red};
+    border-color: ${o.red};
+    color: ${o.red};
+
+    :focus {
+        box-shadow: 0px 0px 0px 2px ${o.paleCerulean};
+    }
+
+    svg {
+        fill: ${o.red};
+    }
+
+    ${t}
+`,$=r`
+    background-color: ${o.green};
+    border-color: ${o.green};
+    color: ${o.green};
+
+    :focus {
+        box-shadow: 0px 0px 0px 2px ${o.paleCerulean};
+    }
+
+    svg {
+        fill: ${o.green};
+    }
+
+    ${t}
+`,u=r`
+    background-color: ${o.blue};
+    border-color: ${o.blue};
+    color: ${o.blue};
+
+    :focus {
+        box-shadow: 0px 0px 0px 2px ${o.paleCerulean};
+    }
+
+    svg {
+        fill: ${o.blue};
+    }
+
+    ${t}
+`,p=r`
+    background-color: ${o.yellow};
+    border-color: ${o.yellow};
+    color: ${o.yellow};
+
+    :focus {
+        box-shadow: 0px 0px 0px 2px ${o.paleCerulean};
+    }
+
+    svg {
+        fill: ${o.yellow};
+    }
+
+    ${t}
+`,x=e=>{switch(e){case"red":return d;case"green":return $;case"blue":return u;case"yellow":return p;default:return}},b=a(s)`
+    :after {
+        background-color: ${l("dark",0)};
+        content: '';
+        display: block;
+        height: 100%;
+        left: 0;
+        pointer-events: none;
+        position: absolute;
+        top: 0;
+        transition: all 0.3s ease;
+        width: 100%;
+        z-index: 0;
+    }
+
+    ${({color:e})=>e&&x(e)}
+`;var g={ButtonColoredStyled:b};const{ButtonColoredStyled:i}=g,h=({color:e,...c})=>n(i,{color:e,...c});export{h as default};
---
+++ assets/ButtonSecondary-BQZr5KHe.js
@@ -0,0 +1,17 @@
+import{B as e,z as o,D as l,E as r}from"./index-ClxLoTQ8.js";const a=e(r)`
+    background-color: ${o.blueLight};
+    border-color: ${o.blueLight};
+    color: ${o.blue};
+
+    :active {
+        border-color: ${o.blue};
+    }
+
+    :focus {
+        box-shadow: 0px 0px 0px 2px ${o.paleCerulean};
+    }
+
+    svg {
+        fill: ${o.blue};
+    }
+`;var s={ButtonSecondaryStyled:a};const{ButtonSecondaryStyled:c}=s,u=({...t})=>l(c,{...t});export{u as default};
