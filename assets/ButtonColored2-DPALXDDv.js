d95489f8c2c4b5a597cb0195a0b4a0bf18690e0b
Prastav <pokharelprastav@gmail.com>
2024-05-20 14:56:53 +0545

    Updates

---
+++ assets/ButtonColored2-DPALXDDv.js
@@ -0,0 +1,89 @@
+import{G as r,H as l,J as o,P as a,Q as n,R as s}from"./index-R8wq8AXP.js";const t=r`
+    :after {
+        background-color: ${l("light",90)};
+    }
+
+    :hover {
+        :after {
+            background-color: ${l("light",70)};
+        }
+    }
+
+    :active {
+        :after {
+            background-color: ${l("light",50)};
+        }
+    }
+`,d=r`
+    background-color: ${o.red};
+    border-color: ${o.red};
+    color: ${o.red};
+
+    :focus {
+        box-shadow: 0px 0px 0px 2px ${o.paleCerulean};
+    }
+
+    svg {
+        fill: ${o.red};
+    }
+
+    ${t}
+`,$=r`
+    background-color: ${o.green};
+    border-color: ${o.green};
+    color: ${o.green};
+
+    :focus {
+        box-shadow: 0px 0px 0px 2px ${o.paleCerulean};
+    }
+
+    svg {
+        fill: ${o.green};
+    }
+
+    ${t}
+`,u=r`
+    background-color: ${o.blue};
+    border-color: ${o.blue};
+    color: ${o.blue};
+
+    :focus {
+        box-shadow: 0px 0px 0px 2px ${o.paleCerulean};
+    }
+
+    svg {
+        fill: ${o.blue};
+    }
+
+    ${t}
+`,p=r`
+    background-color: ${o.yellow};
+    border-color: ${o.yellow};
+    color: ${o.yellow};
+
+    :focus {
+        box-shadow: 0px 0px 0px 2px ${o.paleCerulean};
+    }
+
+    svg {
+        fill: ${o.yellow};
+    }
+
+    ${t}
+`,b=e=>{switch(e){case"red":return d;case"green":return $;case"blue":return u;case"yellow":return p;default:return}},g=a(s)`
+    :after {
+        background-color: ${l("dark",0)};
+        content: '';
+        display: block;
+        height: 100%;
+        left: 0;
+        pointer-events: none;
+        position: absolute;
+        top: 0;
+        transition: all 0.3s ease;
+        width: 100%;
+        z-index: 0;
+    }
+
+    ${({color:e})=>e&&b(e)}
+`;var x={ButtonColoredStyled:g};const{ButtonColoredStyled:i}=x,h=({color:e,...c})=>n(i,{color:e,...c});export{h as default};
---
+++ assets/ButtonTranslucent2-6ic3QHsp.js
@@ -0,0 +1,16 @@
+import{P as s,H as a,J as t,Q as e,R as n}from"./index-R8wq8AXP.js";const l=s(n)`
+    background-color: ${a("dark",20)};
+    color: ${t.white};
+
+    :active {
+        border: 2px solid transparent;
+    }
+
+    :focus {
+        box-shadow: 0px 0px 0px 2px ${t.paleCerulean};
+    }
+
+    svg {
+        fill: ${t.white};
+    }
+`;var r={ButtonTranslucentStyled:l};const{ButtonTranslucentStyled:c}=r,d=({...o})=>e(c,{...o});export{d as default};
